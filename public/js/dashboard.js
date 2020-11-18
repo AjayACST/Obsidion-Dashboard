6a618ed780baf7ec793b9d4af378eb768f45676f
AjayACST <ajay@acst.co.nz>
2020-11-18 18:19:08 +1300

    Added links to header in dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,7 +1,18 @@
 const cookies = new UniversalCookie();
 
 if (!cookies.get('loggedin')) {
-    cookies.set("error", "You need to be logged in to do that.");
+    cookies.set("error", "You need to be logged in to do that.", {maxAge: 2, path: "/"});
     console.log(cookies.get('error'))
     location.replace('/');
 }
+
+const header = {
+    data() {
+        return {
+            username: cookies.get('username'),
+            imgURL: cookies.get('imgurl')
+        }
+    }
+};
+
+Vue.createApp(header).mount("#header")

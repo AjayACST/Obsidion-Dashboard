53665023c9ce63859903bf2c48e23c690692de94
AjayACST <ajay@acst.co.nz>
2020-11-29 14:54:23 +1300

    Fixed Cannot set headers after they are sent to the client error when using database API

--- api/database.js
+++ api/database.js
@@ -53,7 +53,7 @@ router.get('/account', (req, res) => {
             if (row.token === token) {
                 pool.query(`SELECT username FROM discord_user WHERE id='${id}'`)
                 .then(response => res.json({code: 200, "username": response.rows[0].username}))
-                .catch(e => res.json({code: 404, "message": "User has not linked their account."}))
+                .catch(e => console.error(e))
             } else {
                 res.json({code: 401, "message": "That is not a valid token."})
             }
@@ -80,25 +80,23 @@ router.get('/accountUpdate', (req, res) => {
         if (rows.length === 0) {
             res.json({code: 401, "message": "That is not a valid token."})
         }
-
-        rows.forEach((row) => {
-            if (row.token === token) {
-                pool.query(`SELECT username FROM discord_user WHERE id='${id}'`)
-                .then((response) => {
-                    if (response.rows[0]) {
-                        pool.query(`UPDATE discord_user SET username = '${username}' WHERE id='${id}'`)
-                        .then(response => res.json({code: 200, "message": "Successfully updated linked user account."}))
-                        .catch(e => res.json({code: 404, "message": "An error has occurred please try again later."}))
-                    } else {
-                        pool.query(`INSERT INTO discord_user (id, username) VALUES ('${id}', '${username}')`)
-                        .then(response => res.json({code: 200, "message": "Successfully updated linked user account."}))
-                        .catch(e => res.json({code: 404, "message": "An error has occurred please try again later."}))
-                    }
-                })
-            } else {
-                res.json({code: 401, "message": "That is not a valid token."})
-            }
-        })
+        
+        if (row.token === token) {
+            pool.query(`SELECT username FROM discord_user WHERE id='${id}'`)
+            .then((response) => {
+                if (response.rows[0]) {
+                    pool.query(`UPDATE discord_user SET username = '${username}' WHERE id='${id}'`)
+                    .then(response => res.json({code: 200, "message": "Successfully updated linked user account."}))
+                    .catch(e => console.error(e))
+                } else {
+                    pool.query(`INSERT INTO discord_user (id, username) VALUES ('${id}', '${username}')`)
+                    .then(response => res.json({code: 200, "message": "Successfully updated linked user account."}))
+                    .catch(e => console.error(e))
+                }
+            })
+        } else {
+            res.json({code: 401, "message": "That is not a valid token."})
+        }
 
     });
 })
@@ -121,24 +119,23 @@ router.get('/discordGuild', (req, res) => {
             res.json({code: 401, "message": "That is not a valid token."})
         }
 
-        rows.forEach((row) => {
-            if (row.token === token) {
-                fetch(`https://discord.com/api/v6/guilds/${guildID}/members/${clientID}`, {
-                    method: "GET",
-                    headers: {
-                        "Authorization": `Bot ${botToken}`
-                    }
-                }).then(response => response.json()).then((discordResponse) => {
-                    if (discordResponse.message === 'Missing Access') {
-                        res.json({code: 200, 'message': 'Bot is not in that guild.'})
-                    } else {
-                        res.json({code: 200, 'message': 'Bot is in that guild.'})
-                    }
-                })
-            } else {
-                res.json({code: 401, "message": "That is not a valid token."})
-            }
-        })
+        if (rows.token === token) {
+            fetch(`https://discord.com/api/v6/guilds/${guildID}/members/${clientID}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bot ${botToken}`
+                }
+            }).then(response => response.json()).then((discordResponse) => {
+                if (discordResponse.message === 'Missing Access') {
+                    res.json({code: 200, 'message': 'Bot is not in that guild.'})
+                } else {
+                    res.json({code: 200, 'message': 'Bot is in that guild.'})
+                }
+            })
+            .catch(e => console.error(e))
+        } else {
+            res.json({code: 401, "message": "That is not a valid token."})
+        }
 
     });
 })
@@ -160,15 +157,19 @@ router.get('/prefix', (req, res) => {
             res.json({code: 401, "message": "That is not a valid token."})
         }
 
-        rows.forEach((row) => {
-            if (row.token === token) {
-                pool.query(`SELECT prefix FROM guild WHERE id='${guildid}'`)
-                .then(response=> res.json({code: 200, "prefix": response.rows[0].prefix}))
-                .catch(e => res.json({code: 404, "message": "User has not setup a custom prefix."}))
-            } else {
-                res.json({code: 401, "message": "That is not a valid token."})
-            }
-        })
+        if (rows.token === token) {
+            pool.query(`SELECT prefix FROM guild WHERE id='${guildid}'`)
+            .then(response => {
+                if (!response.rows[0]) {
+                    return res.json({code: 404, "message": "User has not setup a custom prefix."})
+                } else {
+                    return res.json({code: 200, "prefix": response.rows[0].prefix})
+                }
+            })
+            .catch(e => console.error(e))
+        } else {
+            res.json({code: 401, "message": "That is not a valid token."})
+        }
 
     });
 })
@@ -194,38 +195,36 @@ router.get('/prefixUpdate', (req, res) => {
             res.json({code: 401, "message": "That is not a valid token."})
         }
 
-        rows.forEach((row) => {
-            if (row.token === token) {
-                pool.query(`SELECT prefix FROM guild WHERE id='${guildid}'`)
-                .then((response) => {
-                    if (response.rows[0]) {
-                        pool.query(`UPDATE guild SET prefix = '${prefix}' WHERE id='${guildid}'`)
-                        .then((response) => {
-                            const client = redis.createClient(redisPort, redisHost, redis);
-                            var key = `prefix_${guildid}`
-                            var prefixUpdate = `["<@!${userid}>", "<@${userid}>", "${prefix}"]`
-                            client.set(key, prefixUpdate, 'EX', 28800, redis.print);
-                            client.quit();
-                            return res.json({code: 200, "message": "Successfully updated linked prefix for guild."})
-                        })
-                        .catch(e => res.json({code: 404, "message": "An error has occurred please try again later."}))
-                    } else {
-                        pool.query(`INSERT INTO guild (id, prefix) VALUES ('${guildid}', '${prefix}')`)
-                        .then((response) => {
-                            const client = redis.createClient(redisPort, redisHost, redis);
-                            var key = `prefix_${guildid}`
-                            var prefixUpdate = `["<@!${userid}>", "<@${userid}>", "${prefix}"]`
-                            client.set(key, prefixUpdate, 'EX', 28800, redis.print);
-                            client.quit();
-                            return res.json({code: 200, "message": "Successfully updated linked prefix for guild."})
-                        })
-                        .catch(e => res.json({code: 404, "message": "An error has occurred please try again later."}))
-                    }
-                })
-            } else {
-                res.json({code: 401, "message": "That is not a valid token."})
-            }
-        })
+        if (row.token === token) {
+            pool.query(`SELECT prefix FROM guild WHERE id='${guildid}'`)
+            .then((response) => {
+                if (response.rows[0]) {
+                    pool.query(`UPDATE guild SET prefix = '${prefix}' WHERE id='${guildid}'`)
+                    .then((response) => {
+                        const client = redis.createClient(redisPort, redisHost, redis);
+                        var key = `prefix_${guildid}`
+                        var prefixUpdate = `["<@!${userid}>", "<@${userid}>", "${prefix}"]`
+                        client.set(key, prefixUpdate, 'EX', 28800, redis.print);
+                        client.quit();
+                        return res.json({code: 200, "message": "Successfully updated linked prefix for guild."})
+                    })
+                    .catch(e => res.json({code: 404, "message": "An error has occurred please try again later."}))
+                } else {
+                    pool.query(`INSERT INTO guild (id, prefix) VALUES ('${guildid}', '${prefix}')`)
+                    .then((response) => {
+                        const client = redis.createClient(redisPort, redisHost, redis);
+                        var key = `prefix_${guildid}`
+                        var prefixUpdate = `["<@!${userid}>", "<@${userid}>", "${prefix}"]`
+                        client.set(key, prefixUpdate, 'EX', 28800, redis.print);
+                        client.quit();
+                        return res.json({code: 200, "message": "Successfully updated linked prefix for guild."})
+                    })
+                    .catch(e => console.error(e))
+                }
+            })
+        } else {
+            res.json({code: 401, "message": "That is not a valid token."})
+        }
 
     });
 })
